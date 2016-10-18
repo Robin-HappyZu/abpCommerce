0738903c862cae9c36f1a5d9fcffec3de30d5f21
RobinZ <94353156@qq.com>
2016-10-18 18:37:05 +0800

    景点详情页展示

---
+++ src/HappyZu.CloudStore.Web/Areas/Mobile/Models/Dest/DetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Abp.Application.Services.Dto;
+using HappyZu.CloudStore.FileManager.Dto;
+using HappyZu.CloudStore.Trip.Dto;
+
+namespace HappyZu.CloudStore.Web.Areas.Mobile.Models.Dest
+{
+    public class DetailViewModel
+    {
+        public DestDto Dest { get; set; }
+
+        public IPagedResult<TicketDto> Tickets { get; set; }
+
+        public IPagedResult<TicketTypeDto> TicketTypes { get; set; }
+        public IPagedResult<FileItemMappingDto> Pictures { get; set; }
+
+        public DestCityDto City { get; set; }
+
+        public DestProvinceDto Province { get; set; }
+    }
+}
