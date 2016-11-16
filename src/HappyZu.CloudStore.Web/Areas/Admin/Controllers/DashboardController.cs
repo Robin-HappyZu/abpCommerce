0bd884f240eb47376ca2ff19a43e72020b91c498
RobinZ <94353156@qq.com>
2016-11-16 15:35:13 +0800

    控制面板统计数据显示

--- src/HappyZu.CloudStore.Web/Areas/Admin/Controllers/DashboardController.cs
+++ src/HappyZu.CloudStore.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Abp.Authorization;
+using Happyzu.CloudStore.Extensions;
 using HappyZu.CloudStore.StatisticalAnalysis;
 using HappyZu.CloudStore.StatisticalAnalysis.Dto;
 using HappyZu.CloudStore.Web.Areas.Admin.Models;
@@ -40,6 +41,35 @@ namespace HappyZu.CloudStore.Web.Areas.Admin.Controllers
                 UserStatisticsByDay = users
             };
             return View(vm);
+        }
+
+        public async Task<JsonResult> GetDashboardOrderStatistics(int count)
+        {
+            Dictionary<string, int> dataOrders = new Dictionary<string, int>();
+            Dictionary<string, decimal> dataAmount = new Dictionary<string, decimal>();
+            var list = await _statisticsAppService.QuerySalesStatisticsByMonth(new QuerySalesStatisticsByMonthInput
+            {
+                MaxResultCount = count
+            });
+            for (int i = count; i >= 1; i--)
+            {
+                var startTime = DateTime.Now.ToMonthStart().AddMonths(-i + 1);
+
+                var key = startTime.ToString("yyyy-MM");
+
+                var value = list.Items.FirstOrDefault(x => x.FirstDayOfMonth == startTime);
+                if (value!=null)
+                {
+                    dataOrders.Add(key, value.Orders);
+                    dataAmount.Add(key, value.Subtotal);
+                }
+                else
+                {
+                    dataOrders.Add(key, 0);
+                    dataAmount.Add(key, 0);
+                }
+            }
+            return Json(new {orders= dataOrders ,amounts= dataAmount }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ src/Happyzu.CloudStore.Common/Utils/DateTimeExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happyzu.CloudStore.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// 一天最开始时间，例：2014-04-05 17：30：20 ，结果 2014-04-05 00：00：00
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static DateTime ToStart(this DateTime value)
+        {
+            return new DateTime(value.Year, value.Month, value.Day, 0, 0, 0);
+        }
+
+        /// <summary>
+        /// 本周最开始时间
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static DateTime ToWeekStart(this DateTime value)
+        {
+            int i = value.DayOfWeek - DayOfWeek.Monday;
+            if (i == -1) i = 6;// i值 > = 0 ，因为枚举原因，Sunday排在最前，此时Sunday-Monday=-1，必须+7=6。   
+            TimeSpan ts = new TimeSpan(i, 0, 0, 0);
+            return value.Subtract(ts).ToStart();
+        }
+
+        /// <summary>
+        /// 本月最开始时间
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static DateTime ToMonthStart(this DateTime value)
+        {
+            return new DateTime(value.Year, value.Month, 1, 0, 0, 0);
+        }
+
+        /// <summary>
+        /// 本月最后时间
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static DateTime ToMonthEnd(this DateTime value)
+        {
+            return value.ToStart().AddMonths(1).AddSeconds(-1);
+        }
+    }
+}
